5b835349c103e30e15d18e5161aa1e252b4221df
vitalijtverskoj <vitalijtverskoj@gmail.com>
2024-11-06 20:02:45 +0300

    add task01, 02, 03

---
+++ lesson_02/task_01.py
@@ -0,0 +1,19 @@
+# Задание №1.
+# Создайте несколько переменных разных типов.
+# Проверьте к какому типу относятся созданные переменные.
+
+num = 9
+string = 'hello'
+bool_type = True
+float_type = 3.14
+collect = (num,)
+list_1 = [string,]
+dict_1 = {1: bool_type}
+
+print(type(num))
+print(type(string))
+print(type(bool_type))
+print(type(float_type))
+print(type(collect))
+print(type(list_1))
+print(type(dict_1))
